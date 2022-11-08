10c7a106f8ca8824febeb43a9424c73f1cac7361
Agnieszka Syrek <agnieszkasyrek@MacBook-Pro-Jakub.local>
2022-11-08 13:01:59 +0100

    Adding the ability for the player to choose the type of dice

--- 2001.py
+++ 2001.py
@@ -1,43 +1,67 @@
 import random
 
 
+def type_of_dice():
+    """The function takes the cube type as string from the user
+    and returns the cube type as int
+    :rtype: int
+    :return type of dice"""
+    while True:
+        type_of_dices = ['D3', 'D4', 'D6', 'D8', 'D10', 'D12', 'D20', 'D100']
+        dice_type = input('Choose the type of dice to play: D3, D4, D6, D8, D10, D12, D20, D100 ')
+        if dice_type in type_of_dices:
+            dice_type_array = dice_type[1:]
+            dice_type = ''.join(dice_type_array)
+            dice_type = int(dice_type)
+            break
+        else:
+            print('You entered the wrong type of dice')
+    return dice_type
+
+
+def user_throw():
+    dice_type1 = type_of_dice()
+    dice_type2 = type_of_dice()
+    throw = random.randint(1, dice_type1) + random.randint(1, dice_type2)
+    return throw
+
+
+def comp_throw():
+    type_of_dices = [3, 4, 6, 8, 10, 12, 20, 100]
+    dice_type1 = random.choice(type_of_dices)
+    dice_type2 = random.choice(type_of_dices)
+    throw = random.randint(1, dice_type1) + random.randint(1, dice_type2)
+    return throw
+
+
 def game():
-    gamer1 = 0
-    gamer2 = 0
-    throw_dice = input('Throw the dices ')
-    throw1 = random.randint(1, 6) + random.randint(1, 6)
-    throw2 = random.randint(1, 6) + random.randint(1, 6)
-    gamer1 = gamer1 + throw1
-    gamer2 = gamer2 + throw2
-    print(f'Gamer 1 points: {gamer1}')
-    print(f'Gamer 2 points: {gamer2}')
-    while gamer1 < 2001 and gamer2 < 2001:
-        throw_dice = input('Throw the dices ')
-        throw1 = random.randint(1, 6) + random.randint(1, 6)
-        throw2 = random.randint(1, 6) + random.randint(1, 6)
-        # print(f'throw1 {throw1}')
-        # print(f'throw2 {throw2}')
-
-        if throw1 == 7:
-            gamer1 = gamer1 // 7
-        elif throw1 == 11:
-            gamer1 = gamer1 * 11
+    user_points = user_throw()
+    comp_points = comp_throw()
+    print(f'Your points: {user_points}')
+    print(f'Computer points: {comp_points}')
+    while user_points < 2001 and comp_points < 2001:
+        throw_user = user_throw()
+        throw_comp = comp_throw()
+        if throw_user == 7:
+            user_points = user_points // 7
+        elif throw_user == 11:
+            user_points = user_points * 11
         else:
-            gamer1 = gamer1 + throw1
-        print(f'Gamer 1 points: {gamer1}')
+            user_points = user_points + throw_user
+        print(f'Your points: {user_points}')
 
-        if throw2 == 7:
-            gamer2 = gamer2 // 7
-        elif throw2 == 11:
-            gamer2 = gamer2 * 11
+        if throw_comp == 7:
+            comp_points = comp_points // 7
+        elif throw_comp == 11:
+            comp_points = comp_points * 11
         else:
-            gamer2 = gamer2 + throw2
-        print(f'Gamer 2 points: {gamer2}')
+            comp_points = comp_points + throw_comp
+        print(f'Computer points: {comp_points}')
 
-    if gamer1 > 2001 and gamer2 < 2001:
-        print('The winner is: Gamer 1')
-    elif gamer1 < 2001 and gamer2 > 2001:
-        print('The winner is: Gamer 2')
+    if user_points > 2001 and comp_points < 2001:
+        print('You won')
+    elif user_points < 2001 and comp_points > 2001:
+        print('Computer won')
     else:
         print('Draw')
 
